676c600d4b5cd0df493a8c382f5fa95d5a81eb93
Gekire156 <gavin.letulle@icloud.com>
2024-03-25 12:06:41 -0500

    Player attempted

--- app/src/main/java/cs2/game/Player.java
+++ app/src/main/java/cs2/game/Player.java
@@ -11,21 +11,31 @@ public class Player extends Sprite {
   //**Remember that some fields are inherited from Sprite
   public Player(Image avatar, Image bullPic, Vec2 p) { }
   */
-
+  public Player(Image avatar, Image bullPic, Vec2 p) {
+    super(avatar, p);
+    bulletPicture = bullPic;
+  }
   /*
   // This method should create a new Bullet object and return it
   // The Bullet should be initialized with the bulletPicture, the
   // current position of the player, and a velocity going up the screen
   public Bullet shoot() { }
   */
-
+  public Bullet shoot(){
+    return new Bullet(bulletPicture, pos, new Vec2(0, 50));
+  }
   /*
   // This method should move the player left by some amount
   public void moveLeft() { }
   */
-
+  public void moveLeft(){
+    move(new Vec2(-1, 0));
+  }
   /*
   // This method should move the player right by some amount
   public void moveRight() { }
   */
+  public void moveRight(){
+    move(new Vec2(1, 0));
+  }
 }
